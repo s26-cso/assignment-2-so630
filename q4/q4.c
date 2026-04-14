f8b6788f4c4d35fc48cd2f1b486cf7d2d3d9e580
so630 <soham0511@gmail.com>
2026-04-14 21:26:00 +0530

    finish q4

---
+++ q4/q4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dlfcn.h>
+
+typedef int (*fptr)(int, int);
+
+int main()
+{
+    char op[10];
+    int a,b;
+
+    while (scanf("%s %d %d", op, &a, &b) == 3)
+    {
+        char libname[20];
+        sprintf(libname, "./lib%s.so", op);
+
+        void* handle = dlopen(libname, RTLD_LAZY);
+        if (!handle) {
+            fprintf(stderr, "Error loading %s\n", libname);
+            continue;
+        }
+
+        fptr func = (fptr)dlsym(handle, op);
+        if (!func) {
+            fprintf(stderr, "Error finding function %s\n%s", op, dlerror());
+            dlclose(handle);
+            continue;
+        }
+
+        int result = func(a, b);
+        printf("%d\n", result);
+
+        dlclose(handle);
+    }
+
+    return 0;
+}
